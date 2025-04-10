7759d34c550649531e21d2d68c8b68e818204b1d
Valentina Maraio <valentina.maraio89@gmail.com>
2025-04-10 17:47:20 +0200

    basic dashboard cards no data

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,8 +1,16 @@
+import { SidebarInset } from "@/components/ui/sidebar"
+
 export default function HomePage() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Welcome to your home page!</p>
-      </div>
-    )
-  }
+  return (
+      <SidebarInset>
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <div className="px-4 lg:px-6">
+              </div>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+  )
+}
